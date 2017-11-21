4942350e0a9e1ef7b00a1486168a4ba1a93a5fbe
Vladislav Simic <dovlasim@gmail.com>
2017-11-21 11:52:26 +0100

    Postavljeni inicijalni Adapter i Parser projekti.

---
+++ EnergyManagementSystem/CIMAdapter/Importer/RegulatingControlConverter.cs
@@ -0,0 +1,18 @@
+﻿
+namespace EMS.CIMAdapter.Importer
+{
+    using EMS.Common;
+
+    public static class RegulatingControlConverter
+    {
+
+        #region Populate ResourceDescription
+
+        #endregion Populate ResourceDescription
+
+       
+        #region Enums convert
+
+        #endregion Enums convert
+    }
+}
