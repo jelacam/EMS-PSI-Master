8341980e6e729f9367526d4ca79702593454ff2c
borka94 <borka.pzs@gmail.com>
2017-11-21 23:17:04 +0100

    Inicijalni unit test za IdentifiedObject dodat

---
+++ EnergyManagementSystem/DataModelTest/IdentifiedObjectTest.cs
@@ -0,0 +1,30 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IdentifiedObjectTest.cs" company="EMS-Team">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace DataModelTest
+{
+    using EMS.Services.NetworkModelService.DataModel.Core;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Class for unit testing IdentifiedObject
+    /// </summary>
+    [TestFixture]
+    public class IdentifiedObjectTest
+    {
+        /// <summary>
+        /// Unit test for constructor with parameters
+        /// </summary>
+        /// <param name="globalId">globalId for the constructor</param>
+        [Test]
+        [TestCase(1623)]
+        public void Constructor(long globalId)
+        {
+            IdentifiedObject io = new IdentifiedObject(globalId);
+            Assert.IsNotNull(io);
+        }
+    }
+}
