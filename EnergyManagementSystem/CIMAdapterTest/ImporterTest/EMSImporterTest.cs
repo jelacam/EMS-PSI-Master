3b2aa9663369819de6c97927e1517ff8a52eba99
Marko Jelača <jelaca.marko@gmail.com>
2017-11-25 19:22:18 +0100

    Izmenjen framework na 4.5. Kreiran inicijalni test za EMSImporter.

---
+++ EnergyManagementSystem/CIMAdapterTest/ImporterTest/EMSImporterTest.cs
@@ -0,0 +1,54 @@
+﻿using CIM.Model;
+using EMS.CIMAdapter;
+using EMS.CIMAdapter.Importer;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIMAdapterTest.ImporterTest
+{
+    [TestFixture]
+    public class EMSImporterTest
+    {
+        #region Declarations
+
+        private EMSImporter EMSImporter;
+        private TransformAndLoadReport report;
+        private ConcreteModel concreteModel;
+
+        #endregion Declarations
+
+        #region Setup
+
+        [OneTimeSetUp]
+        public void SetupTest()
+        {
+            EMSImporter = new EMSImporter();
+            concreteModel = new ConcreteModel();
+        }
+
+        #endregion Setup
+
+        #region Tests
+
+        [Test]
+        [TestCase(TestName = "Reset")]
+        public void ResetTest()
+        {
+            //EMSImporter.Reset();
+            //Assert.AreEqual(EMSImporter.Instance.NMSDelta, )
+        }
+
+        [Test]
+        [TestCase(TestName = "CreateNMSDelta")]
+        public void CreateNMSDeltaTest()
+        {
+            Assert.IsNotNull(EMSImporter.Instance.CreateNMSDelta(concreteModel));
+        }
+
+        #endregion Tests
+    }
+}
