e7559f2aa32e752f557f156902c2ffe6fad702f4
Marko Jelaca <jelaca.marko@gmail.com>
2017-12-05 11:49:22 +0100

    Kreiran inicijalni scada commanding service i hostovan.

---
+++ EnergyManagementSystem/SCADACommandingService/SCADACommandingService.cs
@@ -0,0 +1,93 @@
+﻿using EMS.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Services.SCADACommandingService
+{
+    public class SCADACommandingService : IDisposable
+    {
+        private SCADACommanding scadaCMD = null;
+        private List<ServiceHost> hosts = null;
+
+        public SCADACommandingService()
+        {
+            scadaCMD = new SCADACommanding();
+            InitializeHosts();
+        }
+        
+        public void Start()
+        {
+            StartHosts();
+        }
+
+        private void InitializeHosts()
+        {
+            hosts = new List<ServiceHost>();
+            hosts.Add(new ServiceHost(typeof(SCADACommanding)));
+        }
+
+        private void StartHosts()
+        {
+            if(hosts == null || hosts.Count == 0)
+            {
+                throw new Exception("SCADA Commanding Services can not be opend because it is not initialized.");
+            }
+
+            string message = string.Empty;
+            foreach(ServiceHost host in hosts)
+            {
+                host.Open();
+
+                message = string.Format("The WCF service {0} is ready.", host.Description.Name);
+                Console.WriteLine(message);
+                CommonTrace.WriteTrace(CommonTrace.TraceInfo, message);
+
+                foreach(Uri uri in host.BaseAddresses)
+                {
+                    Console.WriteLine(uri);
+                    CommonTrace.WriteTrace(CommonTrace.TraceInfo, uri.ToString());
+                }
+
+                Console.WriteLine("\n");
+            }
+
+           
+
+            message = string.Format("Trace level: {0}", CommonTrace.TraceLevel);
+            Console.WriteLine(message);
+            CommonTrace.WriteTrace(CommonTrace.TraceInfo, message);
+
+
+            message = "The SCADA Commanding Service is started.";
+            Console.WriteLine("\n{0}", message);
+            CommonTrace.WriteTrace(CommonTrace.TraceInfo, message);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CloseHost()
+        {
+            if (hosts == null || hosts.Count == 0)
+            {
+                throw new Exception("SCADA Commanding Services can not be closed because it is not initialized.");
+            }
+
+            foreach (ServiceHost host in hosts)
+            {
+                host.Close();
+            }
+
+            string message = "The SCADA Commanding Service is closed.";
+            CommonTrace.WriteTrace(CommonTrace.TraceInfo, message);
+            Console.WriteLine("\n\n{0}", message);
+        }
+
+    }
+}
