15f5a055597f5b3f9c0885661a286af3731e1cad
SrdjanPaunovic <srdjan.paunovic94@gmail.com>
2017-12-06 00:06:26 +0100

    ReadCoils for modbus

---
+++ EnergyManagementSystem/SCADACollectingServiceSelfHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using SmoothModbus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCADACollectingServiceSelfHost
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ModbusClient modbusClient = new ModbusClient("localhost", 502);
+			modbusClient.Connect();
+			while (true)
+			{
+				var veqe = modbusClient.ReadCoils(0, 3);
+			}
+		}
+	}
+}
