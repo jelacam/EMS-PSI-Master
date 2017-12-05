67fe9a370d107d584c0ac8ffeae79fd44d49aae2
Marko Jelaca <jelaca.marko@gmail.com>
2017-12-05 12:14:34 +0100

    Kreiran inicjialni scada crunching service i hostovan.

--- EnergyManagementSystem/SCADACrunchingService/SCADACrunchingService.cs
+++ EnergyManagementSystem/SCADACrunchingService/SCADACrunchingService.cs
@@ -6,39 +6,41 @@ using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EMS.Services.SCADACommandingService
+namespace EMS.Services.SCADACrunchingService
 {
-    public class SCADACommandingService : IDisposable
+    public class SCADACrunchingService : IDisposable
     {
-        private SCADACommanding scadaCMD = null;
+        private SCADACrunching scadaCR = null;
         private List<ServiceHost> hosts = null;
 
-        public SCADACommandingService()
+
+        public SCADACrunchingService()
         {
-            scadaCMD = new SCADACommanding();
+            scadaCR = new SCADACrunching();
             InitializeHosts();
         }
-        
+
         public void Start()
         {
             StartHosts();
         }
 
+        
         private void InitializeHosts()
         {
             hosts = new List<ServiceHost>();
-            hosts.Add(new ServiceHost(typeof(SCADACommanding)));
+            hosts.Add(new ServiceHost(typeof(SCADACrunching)));
         }
 
         private void StartHosts()
         {
-            if(hosts == null || hosts.Count == 0)
+            if (hosts == null || hosts.Count == 0)
             {
-                throw new Exception("SCADA Commanding Services can not be opend because it is not initialized.");
+                throw new Exception("SCADA Crunching Services can not be opend because it is not initialized.");
             }
 
             string message = string.Empty;
-            foreach(ServiceHost host in hosts)
+            foreach (ServiceHost host in hosts)
             {
                 host.Open();
 
@@ -46,7 +48,7 @@ namespace EMS.Services.SCADACommandingService
                 Console.WriteLine(message);
                 CommonTrace.WriteTrace(CommonTrace.TraceInfo, message);
 
-                foreach(Uri uri in host.BaseAddresses)
+                foreach (Uri uri in host.BaseAddresses)
                 {
                     Console.WriteLine(uri);
                     CommonTrace.WriteTrace(CommonTrace.TraceInfo, uri.ToString());
@@ -55,29 +57,23 @@ namespace EMS.Services.SCADACommandingService
                 Console.WriteLine("\n");
             }
 
-           
+
 
             message = string.Format("Trace level: {0}", CommonTrace.TraceLevel);
             Console.WriteLine(message);
             CommonTrace.WriteTrace(CommonTrace.TraceInfo, message);
 
 
-            message = "The SCADA Commanding Service is started.";
+            message = "The SCADA Crunching Service is started.";
             Console.WriteLine("\n{0}", message);
             CommonTrace.WriteTrace(CommonTrace.TraceInfo, message);
-        }
-
-        public void Dispose()
-        {
-            CloseHosts();
-            GC.SuppressFinalize(this);
         }
 
         public void CloseHosts()
         {
             if (hosts == null || hosts.Count == 0)
             {
-                throw new Exception("SCADA Commanding Services can not be closed because it is not initialized.");
+                throw new Exception("SCADA Crunching Services can not be closed because it is not initialized.");
             }
 
             foreach (ServiceHost host in hosts)
@@ -85,10 +81,15 @@ namespace EMS.Services.SCADACommandingService
                 host.Close();
             }
 
-            string message = "The SCADA Commanding Service is closed.";
+            string message = "The SCADA Crunching Service is closed.";
             CommonTrace.WriteTrace(CommonTrace.TraceInfo, message);
             Console.WriteLine("\n\n{0}", message);
         }
 
+        public void Dispose()
+        {
+            CloseHosts();
+            GC.SuppressFinalize(this);
+        }
     }
 }
