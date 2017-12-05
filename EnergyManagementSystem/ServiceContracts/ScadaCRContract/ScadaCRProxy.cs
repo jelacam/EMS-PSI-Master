1df3268dd09f7b9fe75e114cae96ab68a40a39f4
Marko Jelača <jelaca.marko@gmail.com>
2017-12-05 19:48:39 +0100

    Implementirane (delimicno) proxy klase za kontrakte, dodat ServiceContract atribut na kontrakt interfejse. Izmenjen dijagram komponenti.

--- EnergyManagementSystem/ServiceContracts/ScadaCRContract/ScadaCRProxy.cs
+++ EnergyManagementSystem/ServiceContracts/ScadaCRContract/ScadaCRProxy.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EMS.ServiceContracts
 {
-    public class ScadaCRProxy
+    public class ScadaCRProxy : IScadaCRContract, IDisposable
     {
+        private static IScadaCRContract proxy;
+        private static ChannelFactory<IScadaCRContract> factory;
+
+        public static IScadaCRContract Instance
+        {
+            get
+            {
+                if (proxy == null)
+                {
+                    factory = new ChannelFactory<IScadaCRContract>("*");
+                    proxy = factory.CreateChannel();
+                }
+
+                return proxy;
+            }
+            set
+            {
+                if (proxy == null)
+                {
+                    proxy = value;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            if (factory != null)
+            {
+                factory = null;
+            }
+        }
     }
 }
