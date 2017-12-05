7507cb1713ce738b4ec87ae71500379b92c26980
SrdjanPaunovic <srdjan.paunovic94@gmail.com>
2017-12-05 13:12:12 +0100

    UI fix starting

---
+++ EnergyManagementSystem/UIClient/ViewModel/ViewModelBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIClient.ViewModel
+{
+	public abstract class ViewModelBase:INotifyPropertyChanged,IDisposable
+	{
+		private bool disposed = false;
+
+		protected bool Disposed { get { return disposed; } }
+
+		/// <summary>
+		/// Invoked when this object is being removed from the application
+		/// and will be subject to garbage collection.
+		/// </summary>
+		public virtual void Dispose()
+		{
+			GC.SuppressFinalize(this);
+		}
+
+		#region NotifyPropertyChanged
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		public void OnPropertyChanged(params string[] propertyNames)
+		{
+			foreach (string propertyName in propertyNames)
+			{
+				OnPropertyChanged(propertyName);
+			}
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Child classes can override this method to perform
+		/// clean-up logic, such as removing event handlers.
+		/// </summary>
+		protected virtual void OnDispose()
+		{
+		}
+	}
+}
