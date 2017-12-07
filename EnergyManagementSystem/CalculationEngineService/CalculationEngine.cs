4f41b13bba0a83af4474dfa0f99338d9df0360db
borka94 <borka.pzs@gmail.com>
2017-12-07 12:24:33 +0100

    implementiran CalculationEngineTest

--- EnergyManagementSystem/CalculationEngineService/CalculationEngine.cs
+++ EnergyManagementSystem/CalculationEngineService/CalculationEngine.cs
@@ -9,31 +9,39 @@ namespace EMS.Services.CalculationEngineService
     using System.Collections.Generic;
     using CommonMeasurement;
 
-    /// <summary>
-    /// Class for CalculationEngine
-    /// </summary>
-    public class CalculationEngine
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CalculationEngine" /> class
-        /// </summary>
-        public CalculationEngine()
-        {
-        }
+	/// <summary>
+	/// Class for CalculationEngine
+	/// </summary>
+	public class CalculationEngine
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CalculationEngine" /> class
+		/// </summary>
+		public CalculationEngine()
+		{
+		}
 
-        /// <summary>
-        /// Optimization algorithm
-        /// </summary>
-        /// <param name="measurements">list of measurements which should be optimized</param>
-        /// <returns>returns true if optimization was successful</returns>
-        public bool Optimize(List<MeasurementUnit> measurements)
-        {
-            for (int i = 0; i < measurements.Count; i++)
-            {
-                measurements[i].CurrentValue = measurements[i].CurrentValue * 2;
-            }
+		/// <summary>
+		/// Optimization algorithm
+		/// </summary>
+		/// <param name="measurements">list of measurements which should be optimized</param>
+		/// <returns>returns true if optimization was successful</returns>
+		public bool Optimize(List<MeasurementUnit> measurements)
+		{
+			bool result = false;
+			if (measurements != null)
+			{
+				if (measurements.Count > 0)
+				{
+					for (int i = 0; i < measurements.Count; i++)
+					{
+						measurements[i].CurrentValue = measurements[i].CurrentValue * 2;
+					}
 
-            return true;
+					result= true;
+				}
+			}
+			return result;
         }
     }
 }
