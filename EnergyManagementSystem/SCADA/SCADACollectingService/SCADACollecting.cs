02820aa52f9324db1173a3e7f8fcf51119aac18c
SrdjanPaunovic <srdjan.paunovic94@gmail.com>
2017-12-10 14:53:06 +0100

    Started implementing ModbusHelper for easy converting

--- EnergyManagementSystem/SCADA/SCADACollectingService/SCADACollecting.cs
+++ EnergyManagementSystem/SCADA/SCADACollectingService/SCADACollecting.cs
@@ -12,26 +12,26 @@ namespace EMS.Services.SCADACollectingService
 	/// SCADACollecting component logic
 	/// </summary>
 	public class SCADACollecting : IScadaCLContract
-    {
+	{
 
 		ModbusClient modbusClient;
 
 		public SCADACollecting()
 		{
-			modbusClient = new ModbusClient("localhost",502);
+			modbusClient = new ModbusClient("localhost", 502);
 			modbusClient.Connect();
 		}
 
-        /// <summary>
-        /// Method for getting data values from simulator
-        /// </summary>
-        /// <returns> true if values are successfully returned </returns>
-        public bool GetDataFromSimulator()
-        {
+		/// <summary>
+		/// Method for getting data values from simulator
+		/// </summary>
+		/// <returns> true if values are successfully returned </returns>
+		public bool GetDataFromSimulator()
+		{
 			var values = modbusClient.ReadHoldingRegisters(0, 5);
 			ScadaCRProxy.Instance.SendValues(values);
 
 			return true;
-        }
-    }
+		}
+	}
 }
--- EnergyManagementSystem/SCADA/SCADACrunchingService/SCADACrunching.cs
+++ EnergyManagementSystem/SCADA/SCADACrunchingService/SCADACrunching.cs
@@ -17,6 +17,18 @@ namespace EMS.Services.SCADACrunchingService
 			string function = Enum.GetName(typeof(FunctionCode), value[0]);
 			Console.WriteLine("Function executed: {0}", function);
 
+			int arrayLength = value[1];
+			Console.WriteLine("Byte count: {0}", arrayLength);
+
+			// startIndex = 2 because first two bytes a metadata
+			int[] values = ModbusHelper.GetValueFromByteArray<int>(value, arrayLength, 2);
+			Console.Write("Array: ");
+			foreach(int v in values)
+			{
+				Console.Write(" " + v);
+			}
+			Console.WriteLine();
+
 			return true;
         }
 
