2842c9c8973a2dd3df9dc0b2f9f99bd9c8efc18e
Dusan Jeftic <dusan.jeftic@outlook.com>
2017-12-14 22:28:00 +0100

    Hostovan Transaction Manager servis.

---
+++ EnergyManagementSystem/Transaction/TransactionManager/Program.cs
@@ -0,0 +1,44 @@
+﻿using EMS.Common;
+using EMS.Services.TransactionManagerService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransactionManager
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string message = "Starting Transaction Manager Service ...";
+                CommonTrace.WriteTrace(CommonTrace.TraceInfo, message);
+                Console.WriteLine("\n{0}\n", message);
+
+                using (TransactionManagerService tmService = new TransactionManagerService())
+                {
+                    tmService.Start();
+
+                    message = "Press <Enter> to stop the service.";
+                    CommonTrace.WriteTrace(CommonTrace.TraceInfo, message);
+                    Console.WriteLine(message);
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Transaction Manager Service failed.");
+                Console.WriteLine(ex.StackTrace);
+                CommonTrace.WriteTrace(CommonTrace.TraceError, ex.Message);
+                CommonTrace.WriteTrace(CommonTrace.TraceError, "Transaction Manager Service failed.");
+                CommonTrace.WriteTrace(CommonTrace.TraceError, ex.StackTrace);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ EnergyManagementSystem/Transaction/TransactionManagerService/TransactionManager.cs
@@ -0,0 +1,17 @@
+﻿using EMS.ServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Services.TransactionManagerService
+{
+    public class TransactionManager : ITransactionContract
+    {
+        public void Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
