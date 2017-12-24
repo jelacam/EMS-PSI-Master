b5435875f73d6f77891908ef5b6ed777ba9a68b5
SrdjanPaunovic <srdjan.paunovic94@gmail.com>
2017-12-24 23:09:47 +0100

    Improve scaling factor

--- EnergyManagementSystem/Common/ConsoleOptions.cs
+++ EnergyManagementSystem/Common/ConsoleOptions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -15,11 +16,13 @@ namespace EMS.Common
 			Console.BufferHeight = 1;
 			Console.BackgroundColor = ConsoleColor.Black;
 			Console.ForegroundColor = consoleForegroundColor;
+			double scalingFactorX = GetScalingFactorX();
+			double scalingFactorY = scalingFactorX;
 
-			int consoleWidth = (int)(SystemParameters.FullPrimaryScreenWidth / numberOfColumns) + 5;
-			int consoleHeight = (int)(SystemParameters.FullPrimaryScreenHeight / numerOfRows) + 5;
-			int consoleX = (int)(column * SystemParameters.FullPrimaryScreenWidth / numberOfColumns) ;
-			int consoleY = (int)(row * SystemParameters.FullPrimaryScreenHeight / numerOfRows);
+			int consoleWidth = (int)(SystemParameters.FullPrimaryScreenWidth * scalingFactorX / numberOfColumns) + 5;
+			int consoleHeight = (int)(SystemParameters.FullPrimaryScreenHeight * scalingFactorY / numerOfRows) + 5;
+			int consoleX = (int)(column * SystemParameters.FullPrimaryScreenWidth * scalingFactorX / numberOfColumns) ;
+			int consoleY = (int)(row * SystemParameters.FullPrimaryScreenHeight * scalingFactorY / numerOfRows);
 
 			SetWindowPosition(consoleX, consoleY, consoleWidth, consoleHeight);
 		}
@@ -49,6 +52,40 @@ namespace EMS.Common
 			{
 				return GetConsoleWindow();
 			}
+		}
+
+		[DllImport("gdi32.dll")]
+		static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+		public enum DeviceCap
+		{
+			DESKTOPVERTRES = 117,
+			DESKTOPHORZRES = 118
+			// http://pinvoke.net/default.aspx/gdi32/GetDeviceCaps.html
+		}
+
+
+		public static float GetScalingFactorY()
+		{
+			Graphics g = Graphics.FromHwnd(IntPtr.Zero);
+			IntPtr desktop = g.GetHdc();
+			int LogicalScreenHeight = (int)SystemParameters.FullPrimaryScreenHeight;
+			int PhysicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.DESKTOPVERTRES);
+
+			float ScreenScalingFactor = (float)PhysicalScreenHeight / (float)LogicalScreenHeight;
+
+			return ScreenScalingFactor; // 1.25 = 125%
+		}
+
+		public static float GetScalingFactorX()
+		{
+			Graphics g = Graphics.FromHwnd(IntPtr.Zero);
+			IntPtr desktop = g.GetHdc();
+			int LogicalScreenWidth = (int)SystemParameters.FullPrimaryScreenWidth;
+			int PhysicalScreenWidth = GetDeviceCaps(desktop, (int)DeviceCap.DESKTOPHORZRES);
+
+			float ScreenScalingFactor = (float)PhysicalScreenWidth / (float)LogicalScreenWidth;
+
+			return ScreenScalingFactor; // 1.25 = 125%
 		}
 		#endregion
 	}
