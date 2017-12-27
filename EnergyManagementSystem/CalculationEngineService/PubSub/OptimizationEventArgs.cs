b55380db7e6d83b080076ee530ee2e66ff9294a5
Marko Jelača <jelaca.marko@gmail.com>
2017-12-27 23:57:06 +0100

    Kreiran kontrakt za CE pubsub.

---
+++ EnergyManagementSystem/CalculationEngineService/PubSub/OptimizationEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EMS.Services.CalculationEngineService.PubSub
+{
+    public class OptimizationEventArgs : EventArgs
+    {
+        private int optimizationResult;
+        private string message;
+
+        public int OptimizationResult
+        {
+            get { return optimizationResult; }
+            set { optimizationResult = value; }
+        }
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+    }
+}
---
+++ EnergyManagementSystem/ServiceContracts/CEPubSubContract/ICePubSubContract.cs
@@ -0,0 +1,35 @@
+﻿using System.ServiceModel;
+
+namespace EMS.ServiceContracts
+{
+    /// <summary>
+    /// Publish Subscribe pattern - interface defines methods for subscribe and unsubscribe to a specific topic
+    /// in this case it will be optimization result
+    /// </summary>
+    [ServiceContract(SessionMode = SessionMode.Required, CallbackContract = typeof(ICePubSubCallbackContract))]
+    public interface ICePubSubContract
+    {
+        [OperationContract(IsOneWay = false, IsInitiating = true)]
+        void Subscribe();
+
+        [OperationContract(IsOneWay = false, IsTerminating = true)]
+        void Unsubscribe();
+
+        /// <summary>
+        /// Opeation which the data source program calls to provide the service with new information
+        /// </summary>
+        /// <param name="result"></param>
+        [OperationContract(IsOneWay = false)]
+        void PublishOptimizationResults(float result);
+    }
+
+    /// <summary>
+    /// Communication between service and client is duplex, so this interface represent a callback contract whitch
+    /// the service uses to pass new information to subscribed list of clients.
+    /// </summary>
+    public interface ICePubSubCallbackContract
+    {
+        [OperationContract(IsOneWay = false)]
+        void OptimizationResults(float result);
+    }
+}
