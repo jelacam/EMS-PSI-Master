43d3ab78d4deb88806347e9d52d5676094fee57d
Vladislav Simic <dovlasim@gmail.com>
2018-02-01 19:23:03 +0100

    UI production part

---
+++ EnergyManagementSystem/UIClient/Converters/PeriodEnumToStringConverter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using UIClient.Model;
+
+namespace UIClient.Converters
+{
+	public class PeriodEnumToStringConverter : IValueConverter
+	{
+		private string GetEnumDescription(Enum enumObj)
+		{
+			FieldInfo fieldInfo = enumObj.GetType().GetField(enumObj.ToString());
+
+			object[] attribArray = fieldInfo.GetCustomAttributes(false);
+
+			if (attribArray.Length == 0)
+			{
+				return enumObj.ToString();
+			}
+			else
+			{
+				DescriptionAttribute attrib = null;
+
+				foreach (var att in attribArray)
+				{
+					if (att is DescriptionAttribute)
+						attrib = att as DescriptionAttribute;
+				}
+
+				if (attrib != null)
+					return attrib.Description;
+
+				return enumObj.ToString();
+			}
+		}
+
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			if (value == null)
+			{
+				return "";
+			}
+
+			Enum myEnum = (Enum)value;
+			string description = GetEnumDescription(myEnum);
+			return description;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return Enum.ToObject(targetType, value);
+		}
+	}
+}
--- EnergyManagementSystem/UIClient/ViewModel/HistoryViewModel.cs
+++ EnergyManagementSystem/UIClient/ViewModel/HistoryViewModel.cs
@@ -5,77 +5,74 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Input;
+using UIClient.Model;
 using UIClient.View;
 
 namespace UIClient.ViewModel
 {
-    public class HistoryViewModel : ViewModelBase
-    {
-        private ICommand showDataCommand;
-        private string generatorGid;
-        private DateTime startTime;
-        private DateTime endTime;
-        private List<Property> generatorsGids;
-        private List<Tuple<double, DateTime>> measurements;
-		private ObservableCollection<KeyValuePair<long, List<Tuple<double,DateTime>>>> generatorsContainer = new ObservableCollection<KeyValuePair<long, List<Tuple<double,DateTime>>>>();
+	public class HistoryViewModel : ViewModelBase
+	{
+		private ICommand showDataCommand;
+		private string generatorGid;
+		private DateTime startTime;
+		private DateTime endTime;
+		private List<Tuple<double, DateTime>> measurements;
+		private ObservableCollection<KeyValuePair<long, ObservableCollection<Tuple<double, DateTime>>>> generatorsContainer = new ObservableCollection<KeyValuePair<long, ObservableCollection<Tuple<double, DateTime>>>>();
 		private ICommand visibilityCheckedCommand;
 		private ICommand visibilityUncheckedCommand;
+		private ICommand allGeneratorsCheckedCommand;
+		private ICommand allGeneratorsUnheckedCommand;
+		private ICommand selectedPeriodCommand;
+		private ICommand expandSeparateGensCommand;
 		private Dictionary<long, bool> gidToBoolMap = new Dictionary<long, bool>();
+		private PeriodValues selectedPeriod;
+		private bool isSeparateGensExpanded = false;
 
 		private ModelResourcesDesc modelResourcesDesc;
-        private List<ModelCode> properties;
-        private int iteratorId;
-        private int resourcesLeft;
-        private int numberOfResources = 2;
-        private List<ResourceDescription> retList;
-        private static List<ResourceDescription> internalSynchMachines;
+		private List<ModelCode> properties;
+		private int iteratorId;
+		private int resourcesLeft;
+		private int numberOfResources = 2;
+		private List<ResourceDescription> retList;
+		private static List<ResourceDescription> internalSynchMachines;
 
 
-        public HistoryViewModel(HistoryView mainWindow)
-        {
-            startTime = DateTime.Now;
-            endTime = DateTime.Now;
-            generatorsGids = new List<Property>();
+		public HistoryViewModel(HistoryView mainWindow)
+		{
+			startTime = DateTime.Now;
+			endTime = DateTime.Now;
 
-            internalSynchMachines = new List<ResourceDescription>(5);
-            modelResourcesDesc = new ModelResourcesDesc();
-            retList = new List<ResourceDescription>(5);
-            properties = new List<ModelCode>(10);
-            ModelCode modelCodeSynchronousMachine = ModelCode.SYNCHRONOUSMACHINE;
-            iteratorId = 0;
-            resourcesLeft = 0;
-            numberOfResources = 2;
-            string message = string.Empty;
+			internalSynchMachines = new List<ResourceDescription>(5);
+			modelResourcesDesc = new ModelResourcesDesc();
+			retList = new List<ResourceDescription>(5);
+			properties = new List<ModelCode>(10);
+			ModelCode modelCodeSynchronousMachine = ModelCode.SYNCHRONOUSMACHINE;
+			iteratorId = 0;
+			resourcesLeft = 0;
+			numberOfResources = 2;
+			string message = string.Empty;
 
 
-            // getting SynchronousMachine
-            try
-            {
-                // first get all synchronous machines from NMS
-                properties = modelResourcesDesc.GetAllPropertyIds(modelCodeSynchronousMachine);
-                iteratorId = NetworkModelGDAProxy.Instance.GetExtentValues(modelCodeSynchronousMachine, properties);
-                resourcesLeft = NetworkModelGDAProxy.Instance.IteratorResourcesLeft(iteratorId);
+			// getting SynchronousMachine
+			try
+			{
+				// first get all synchronous machines from NMS
+				properties = modelResourcesDesc.GetAllPropertyIds(modelCodeSynchronousMachine);
+				iteratorId = NetworkModelGDAProxy.Instance.GetExtentValues(modelCodeSynchronousMachine, properties);
+				resourcesLeft = NetworkModelGDAProxy.Instance.IteratorResourcesLeft(iteratorId);
 
-                while (resourcesLeft > 0)
-                {
-                    List<ResourceDescription> rds = NetworkModelGDAProxy.Instance.IteratorNext(numberOfResources, iteratorId);
-                    retList.AddRange(rds);
-                    resourcesLeft = NetworkModelGDAProxy.Instance.IteratorResourcesLeft(iteratorId);
-                }
-                NetworkModelGDAProxy.Instance.IteratorClose(iteratorId);
+				while (resourcesLeft > 0)
+				{
+					List<ResourceDescription> rds = NetworkModelGDAProxy.Instance.IteratorNext(numberOfResources, iteratorId);
+					retList.AddRange(rds);
+					resourcesLeft = NetworkModelGDAProxy.Instance.IteratorResourcesLeft(iteratorId);
+				}
+				NetworkModelGDAProxy.Instance.IteratorClose(iteratorId);
 
-                // add synchronous machines to internal collection
-                internalSynchMachines.AddRange(retList);
+				// add synchronous machines to internal collection
+				internalSynchMachines.AddRange(retList);
 
-                foreach (ResourceDescription rd in internalSynchMachines)
-                {
-                    if (rd.ContainsProperty(ModelCode.IDENTIFIEDOBJECT_GID))
-                    {
-                        generatorsGids.Add(rd.GetProperty(ModelCode.IDENTIFIEDOBJECT_GID));
-                    }
-                }
-
-				foreach(ResourceDescription rd in internalSynchMachines)
+				foreach (ResourceDescription rd in internalSynchMachines)
 				{
 					if (rd.ContainsProperty(ModelCode.IDENTIFIEDOBJECT_GID))
 					{
@@ -83,43 +80,72 @@ namespace UIClient.ViewModel
 						var keyValuePair = GeneratorsContainer.FirstOrDefault(x => x.Key == gid);
 						if (keyValuePair.Value == null)
 						{
-							GeneratorsContainer.Add(new KeyValuePair<long, List<Tuple<double, DateTime>>>(gid, new List<Tuple<double, DateTime>>()));
+							GeneratorsContainer.Add(new KeyValuePair<long, ObservableCollection<Tuple<double, DateTime>>>(gid, new ObservableCollection<Tuple<double, DateTime>>()));
 							GidToBoolMap.Add(gid, false);
 						}
 					}
 				}
-				GeneratorsContainer.ElementAt(0).Value.Add(new Tuple<double, DateTime>(2.5, DateTime.Now.AddSeconds(-3)));
-				GeneratorsContainer.ElementAt(0).Value.Add(new Tuple<double, DateTime>(4.5, DateTime.Now.AddSeconds(-1)));
-
-				OnPropertyChanged(nameof(GeneratorsGids));
 				OnPropertyChanged(nameof(GeneratorsContainer));
 
-            }
-            catch (Exception e)
-            {
-                message = string.Format("Getting extent values method failed for {0}.\n\t{1}", modelCodeSynchronousMachine, e.Message);
-                Console.WriteLine(message);
-                CommonTrace.WriteTrace(CommonTrace.TraceError, message);
-                //return false;
-            }
+			}
+			catch (Exception e)
+			{
+				message = string.Format("Getting extent values method failed for {0}.\n\t{1}", modelCodeSynchronousMachine, e.Message);
+				Console.WriteLine(message);
+				CommonTrace.WriteTrace(CommonTrace.TraceError, message);
+				//return false;
+			}
 
-            // clear retList for getting new model from NMS
-            retList.Clear();
-        }
+			// clear retList for getting new model from NMS
+			retList.Clear();
+		}
 
-        #region Commands
+		#region Commands
 
-        public ICommand ShowDataCommand => showDataCommand ?? (showDataCommand = new RelayCommand(ShowDataCommandExecute));
+		public ICommand ShowDataCommand => showDataCommand ?? (showDataCommand = new RelayCommand(ShowDataCommandExecute));
 
 		public ICommand VisibilityCheckedCommand => visibilityCheckedCommand ?? (visibilityCheckedCommand = new RelayCommand<long>(VisibilityCheckedCommandExecute));
 
 		public ICommand VisibilityUncheckedCommand => visibilityUncheckedCommand ?? (visibilityUncheckedCommand = new RelayCommand<long>(VisibilityUncheckedCommandExecute));
 
+		public ICommand AllGeneratorsCheckedCommand => allGeneratorsCheckedCommand ?? (allGeneratorsCheckedCommand = new RelayCommand(AllGeneratorsCheckedCommandExecute));
+
+		public ICommand AllGeneratorsUncheckedCommand => allGeneratorsUnheckedCommand ?? (allGeneratorsUnheckedCommand = new RelayCommand(AllGeneratorsUnheckedCommandExecute));
+
+		public ICommand ChangePeriodCommand => selectedPeriodCommand ?? (selectedPeriodCommand = new RelayCommand(SelectedPeriodCommandExecute));
+
+		public ICommand ExpandSeparateGensCommand => expandSeparateGensCommand ?? (expandSeparateGensCommand = new RelayCommand(ExpandSeparateGensCommandExecute));
+
 		#endregion
 
 		#region Properties
 
-		public ObservableCollection<KeyValuePair<long, List<Tuple<double, DateTime>>>> GeneratorsContainer
+		public bool IsSeparateGensExpanded
+		{
+			get
+			{
+				return isSeparateGensExpanded;
+			}
+			set
+			{
+				isSeparateGensExpanded = value;
+				OnPropertyChanged(nameof(IsSeparateGensExpanded));
+			}
+		}
+
+		public PeriodValues SelectedPeriod
+		{
+			get
+			{
+				return selectedPeriod;
+			}
+			set
+			{
+				selectedPeriod = value;
+			}
+		}
+
+		public ObservableCollection<KeyValuePair<long, ObservableCollection<Tuple<double, DateTime>>>> GeneratorsContainer
 		{
 			get
 			{
@@ -133,34 +159,36 @@ namespace UIClient.ViewModel
 		}
 
 		public string GeneratorGid
-        {
-            get { return generatorGid; }
-            set { this.generatorGid = value; }
-        }
+		{
+			get { return generatorGid; }
+			set { this.generatorGid = value; }
+		}
 
-        public List<Tuple<double, DateTime>> Measurements
-        {
-            get { return measurements; }
-            set { measurements = value; }
-        }
+		public List<Tuple<double, DateTime>> Measurements
+		{
+			get { return measurements; }
+			set { measurements = value; }
+		}
 
-        public DateTime StartTime
-        {
-            get { return startTime; }
-            set { startTime = value; }
-        }
+		public DateTime StartTime
+		{
+			get { return startTime; }
+			set
+			{
+				startTime = value;
+				OnPropertyChanged(nameof(StartTime));
+			}
+		}
 
-        public DateTime EndTime
-        {
-            get { return endTime; }
-            set { endTime = value; }
-        }
-
-        public List<Property> GeneratorsGids
-        {
-            get { return generatorsGids; }
-            set { generatorsGids = value; }
-        }
+		public DateTime EndTime
+		{
+			get { return endTime; }
+			set
+			{
+				endTime = value;
+				OnPropertyChanged(nameof(EndTime));
+			}
+		}
 
 		public Dictionary<long, bool> GidToBoolMap
 		{
@@ -180,17 +208,17 @@ namespace UIClient.ViewModel
 		#region Command Executions
 
 		private void ShowDataCommandExecute(object obj)
-        {
+		{
 			if (GidToBoolMap.Count != 0)
 			{
-				List<Tuple<double, DateTime>> tempList;
-				foreach(KeyValuePair<long,bool> keyPair in GidToBoolMap)
+				ObservableCollection<Tuple<double, DateTime>> tempList;
+				foreach (KeyValuePair<long, bool> keyPair in GidToBoolMap)
 				{
 					if (keyPair.Value == true)
 					{
 						try
 						{
-							tempList = CalculationEngineUIProxy.Instance.GetHistoryMeasurements(keyPair.Key, startTime, endTime);
+							tempList = new ObservableCollection<Tuple<double, DateTime>>(CalculationEngineUIProxy.Instance.GetHistoryMeasurements(keyPair.Key, startTime, endTime));
 							if (tempList == null)
 							{
 								continue;
@@ -199,16 +227,16 @@ namespace UIClient.ViewModel
 							var keyPairGenerator = GeneratorsContainer.FirstOrDefault(x => x.Key == keyPair.Key);
 							if (keyPairGenerator.Value == null)
 							{
-								GeneratorsContainer.Add(new KeyValuePair<long, List<Tuple<double, DateTime>>>(keyPair.Key, tempList));
+								GeneratorsContainer.Add(new KeyValuePair<long, ObservableCollection<Tuple<double, DateTime>>>(keyPair.Key, tempList));
 							}
 							else
 							{
 								keyPairGenerator.Value.Clear();
-								foreach(Tuple<double,DateTime> tuple in tempList)
+								foreach (Tuple<double, DateTime> tuple in tempList)
 								{
 									keyPairGenerator.Value.Add(new Tuple<double, DateTime>(tuple.Item1, tuple.Item2));
 								}
-								
+
 							}
 							tempList.Clear();
 							tempList = null;
@@ -217,12 +245,12 @@ namespace UIClient.ViewModel
 						{
 							CommonTrace.WriteTrace(CommonTrace.TraceError, "[HistoryViewModel] Error ShowDataCommandExecute {0}", ex.Message);
 						}
-						
+
 					}
 				}
 				OnPropertyChanged(nameof(GeneratorsContainer));
 			}
-        }
+		}
 
 		private void VisibilityCheckedCommandExecute(long gid)
 		{
@@ -234,6 +262,59 @@ namespace UIClient.ViewModel
 		{
 			GidToBoolMap[gid] = false;
 			OnPropertyChanged(nameof(GidToBoolMap));
+		}
+
+		private void AllGeneratorsCheckedCommandExecute(object obj)
+		{
+			GidToBoolMap = GidToBoolMap.ToDictionary(p => p.Key, p => true);
+			OnPropertyChanged(nameof(GidToBoolMap));
+		}
+
+		private void AllGeneratorsUnheckedCommandExecute(object obj)
+		{
+			GidToBoolMap = GidToBoolMap.ToDictionary(p => p.Key, p => false);
+			OnPropertyChanged(nameof(GidToBoolMap));
+		}
+
+		private void SelectedPeriodCommandExecute(object obj)
+		{
+			if (SelectedPeriod == PeriodValues.Last_Hour)
+			{
+				StartTime = DateTime.Now.AddHours(-1);
+				EndTime = DateTime.Now;
+			}
+			else if (SelectedPeriod == PeriodValues.Today)
+			{
+				StartTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 0, 0, 0);
+				EndTime = DateTime.Now;
+			}
+			else if (SelectedPeriod == PeriodValues.Last_Year)
+			{
+				StartTime = DateTime.Now.AddYears(-1);
+				EndTime = DateTime.Now;
+			}
+			else if (SelectedPeriod == PeriodValues.Last_Month)
+			{
+				StartTime = DateTime.Now.AddMonths(-1);
+				EndTime = DateTime.Now;
+			}
+			else if (SelectedPeriod == PeriodValues.Last_4_Month)
+			{
+				StartTime = DateTime.Now.AddMonths(-4);
+				EndTime = DateTime.Now;
+			}
+		}
+
+		private void ExpandSeparateGensCommandExecute(object obj)
+		{
+			if (IsSeparateGensExpanded)
+			{
+				IsSeparateGensExpanded = false;
+			}
+			else
+			{
+				IsSeparateGensExpanded = true;
+			}
 		}
 
 		#endregion
