eccdfeeb7b4ca82f641b05cf16162c01995ccb62
SrdjanPaunovic <srdjan.paunovic94@gmail.com>
2018-02-12 22:27:54 +0100

    Refaktoring dashboard and nms view

--- EnergyManagementSystem/UIClient/ViewModel/MainWindowViewModel.cs
+++ EnergyManagementSystem/UIClient/ViewModel/MainWindowViewModel.cs
@@ -2,6 +2,7 @@ using EMS.Common;
 using EMS.CommonMeasurement;
 using EMS.ServiceContracts;
 using EMS.Services.NetworkModelService.DataModel.Core;
+using EMS.Services.NetworkModelService.DataModel.Meas;
 using EMS.Services.NetworkModelService.DataModel.Production;
 using EMS.Services.NetworkModelService.DataModel.Wires;
 using System;
@@ -56,7 +57,8 @@ namespace UIClient.ViewModel
 
 				List<ModelCode> properties = new List<ModelCode>(10);
 				ModelCode modelCodeEmsFuel = ModelCode.EMSFUEL;
-				ModelCode modelCodeSynchronousMachine = ModelCode.SYNCHRONOUSMACHINE;
+				ModelCode modelCodeAnalog = ModelCode.ANALOG;
+                ModelCode modelCodeSynchronousMachine = ModelCode.SYNCHRONOUSMACHINE;
 				ModelCode modelCodeEnergyConsumer = ModelCode.ENERGYCONSUMER;
 
 				int iteratorId = 0;
@@ -196,9 +198,49 @@ namespace UIClient.ViewModel
 
 				// clear retList
 				retList.Clear();
-				#endregion
+                #endregion
 
-				return true;
+                #region getting Analog
+                try
+                {
+                    // third get all enenrgy consumers from NMS
+                    properties = modelResourcesDesc.GetAllPropertyIds(modelCodeAnalog);
+
+                    iteratorId = NetworkModelGDAProxy.Instance.GetExtentValues(modelCodeAnalog, properties);
+                    resourcesLeft = NetworkModelGDAProxy.Instance.IteratorResourcesLeft(iteratorId);
+
+                    while (resourcesLeft > 0)
+                    {
+                        List<ResourceDescription> rds = NetworkModelGDAProxy.Instance.IteratorNext(numberOfResources, iteratorId);
+                        retList.AddRange(rds);
+                        resourcesLeft = NetworkModelGDAProxy.Instance.IteratorResourcesLeft(iteratorId);
+                    }
+                    NetworkModelGDAProxy.Instance.IteratorClose(iteratorId);
+                }
+                catch (Exception e)
+                {
+                    message = string.Format("Getting extent values method failed for {0}.\n\t{1}", modelCodeEnergyConsumer, e.Message);
+                    Console.WriteLine(message);
+                    CommonTrace.WriteTrace(CommonTrace.TraceError, message);
+                    return false;
+                }
+
+                foreach (var resDesc in retList)
+                {
+                    if (NmsModelMap.ContainsKey(resDesc.Id))
+                    {
+                        NmsModelMap[resDesc.Id] = ResourcesDescriptionConverter.ConvertTo<Analog>(resDesc);
+                    }
+                    else
+                    {
+                        NmsModelMap.Add(resDesc.Id, ResourcesDescriptionConverter.ConvertTo<Analog>(resDesc));
+                    }
+                }
+
+                // clear retList
+                retList.Clear();
+                #endregion
+                return true;
 			}
 		}
 
