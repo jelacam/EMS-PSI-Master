e9544fcdacedf73a9876c5026ede9fdf2eed8010
SrdjanPaunovic <srdjan.paunovic94@gmail.com>
2018-02-18 17:23:41 +0100

    Add tooltip on realtime dashboard

--- EnergyManagementSystem/SCADA/SCADACollectingServiceSelfHost/Program.cs
+++ EnergyManagementSystem/SCADA/SCADACollectingServiceSelfHost/Program.cs
@@ -9,6 +9,7 @@ namespace SCADACollectingServiceSelfHost
     using System;
     using EMS.Common;
     using EMS.Services.SCADACollectingService;
+    using System.Threading;
 
     /// <summary>
     /// Class for Main method
@@ -30,7 +31,7 @@ namespace SCADACollectingServiceSelfHost
                 string message = "Starting SCADA Collecting Service ...";
                 CommonTrace.WriteTrace(CommonTrace.TraceInfo, message);
                 Console.WriteLine("\n{0}\n", message);
-
+                Thread.Sleep(5000);
                 using (SCADACollectingService scadaCL = new SCADACollectingService())
                 {
                     scadaCL.Start();
---
+++ EnergyManagementSystem/UIClient/Selector/ToolTipTemplateSelector.cs
@@ -0,0 +1,32 @@
+﻿using EMS.Services.NetworkModelService.DataModel.Wires;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UIClient.Selector
+{
+    public class ToolTipTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate SyncMachineTemplate { get; set; }
+        public DataTemplate EnConsumerTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item,
+                   DependencyObject container)
+        {
+            if (item is SynchronousMachine)
+            {
+                return SyncMachineTemplate;
+            }
+            else if (item is EnergyConsumer)
+            {
+                return EnConsumerTemplate;
+            }
+
+            return null;
+        }
+    }
+}
