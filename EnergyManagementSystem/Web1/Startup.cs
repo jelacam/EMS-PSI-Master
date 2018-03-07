18aedb2c2bfd9509bd399576aee375a37b3b63f6
Dusan Jeftic <dusan.jeftic@outlook.com>
2018-03-07 12:33:51 +0100

    Inicijalna proba zbgo realtime-a. Kad nije Service Fabric radi dobro, ali kad je service fabric dobija se erorr HRESULT: 0x80071CC0

---
+++ EnergyManagementSystem/Web1/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetify;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebEMS
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMemoryCache();
+            services.AddSignalR();
+            services.AddDotNetify();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+            app.UseWebSockets();
+            app.UseSignalR(routes => routes.MapDotNetifyHub());
+            app.UseDotNetify();
+
+            app.Run(async (context) =>
+            {
+                using (var reader = new StreamReader(File.OpenRead("wwwroot/index.html")))
+                    await context.Response.WriteAsync(reader.ReadToEnd());
+            });
+        }
+    }
+}
---
+++ EnergyManagementSystem/Web1/webpack.config.js
@@ -0,0 +1,23 @@
+﻿"use strict";
+
+var webpack = require("webpack");
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        filename: "./wwwroot/bundle.js",
+        publicPath: '/dist/'
+    },
+    resolve: {
+        modules: ["src", "node_modules"]
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                query: { presets: ["es2015", "react"] }
+            }
+        ]
+    }
+};
