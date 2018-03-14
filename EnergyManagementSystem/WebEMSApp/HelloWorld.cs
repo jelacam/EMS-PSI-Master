055d7e63b1e747bf57013b078c26d58c1e520545
Dusan Jeftic <dusan.jeftic@outlook.com>
2018-03-14 23:13:23 +0100

    Dodata podrska za realtime

---
+++ EnergyManagementSystem/WebEMSApp/HelloWorld.cs
@@ -0,0 +1,27 @@
+﻿using DotNetify;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebEMSApp
+{
+    public class HelloWorld : BaseVM
+    {
+        private Timer _timer;
+        public string Greetings => "Hello World!";
+        public DateTime ServerTime => DateTime.Now;
+
+        public HelloWorld()
+        {
+            _timer = new Timer(state =>
+            {
+                Changed(nameof(ServerTime));
+                PushUpdates();
+            }, null, 0, 1000);
+        }
+
+        public override void Dispose() => _timer.Dispose();
+    }
+}
--- EnergyManagementSystem/WebEMSApp/Startup.cs
+++ EnergyManagementSystem/WebEMSApp/Startup.cs
@@ -6,29 +6,30 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using DotNetify;
+using Microsoft.Owin;
+using Owin;
 
+[assembly: OwinStartup(typeof(WebEMSApp.Startup))]
 namespace WebEMSApp
 {
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+        //public void ConfigureServices(IServiceCollection services)
+        //{
+        //    services.AddMemoryCache();
+        //    services.AddSignalR();
+        //    services.AddDotNetify();
+        //}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IAppBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.MapSignalR();
+            app.UseDotNetify();
         }
     }
 }
